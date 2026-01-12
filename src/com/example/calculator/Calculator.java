ea8681fdea295f1fb7524d8d29bb157c7ceeaa6c
jhsky3118 <doksakim7@naver.com>
2026-01-12 11:45:21 +0900

    첫 커밋

---
+++ src/com/example/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.example.calculator;
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jeongjihun
+ * Date: 26. 1. 12.
+ * Time: 오전 11:34
+ **/
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Hello, Calculator!");
+    }
+}
