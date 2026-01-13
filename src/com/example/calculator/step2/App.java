873822d5c3b11c16fca984ed4ebe5453598f4aad
jhsky3118 <doksakim7@naver.com>
2026-01-13 17:26:16 +0900

    feat: App 클래스에 Calculator 클래스를 연결하여 입력된 값과 기호에 따라 결과값 출력 기능 구현 성공(But 약간의 오류 존재)

--- src/com/example/calculator/step2/App.java
+++ src/com/example/calculator/step2/App.java
@@ -1,20 +1,42 @@
 package com.example.calculator.step2;
 
 
+import java.util.Scanner;
+
 /**
  * Created by IntelliJ IDEA.
  * User: jeongjihun
  * Date: 26. 1. 13.
  * Time: 오후 3:53
  **/
-
+// ?: 궁금했던 내용
 public class App {
     public static void main(String[] args) {
+        // 인스턴스화
         Calculator cal = new Calculator();
 
-        // 반환값이 잘 나오는지 확인
-        int cal1 = cal.calculate(1, 6, '+');
-        System.out.println("답: " + cal1);
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1 = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = sc.nextInt();
+
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            String input = sc.next();   // ?: sc.nextLine() 을 쓰면 자동으로 엔터 입력되어 다음으로 넘어감
+            char in = input.charAt(0);  // ?: (char) 를 안써도 됨
+
+            // calculate 메서드 사용
+            int result = cal.calculate(num1, num2, in);
+            System.out.println("답: " + result);  // ?: 0으로 나눌 때 or 기호가 잘못되었을 때 메시지와 함께 result 반환값도 출력되는 오류 발생
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String ex = sc.next();
+            if (ex.equals("exit")) {
+                break;  // 반복문 종료(exit) 기능 추가
+            } // 반복문 완성
+        }
 
     }
 }
