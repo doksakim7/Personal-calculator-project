51287c40703b2bb67fc945ccdee154f9deff36a4
jhsky3118 <doksakim7@naver.com>
2026-01-14 14:19:01 +0900

    feat & refactor : Step 2 - 처음과 두 번째 입력 숫자가 아니면 재입력 하는 기능 구현 & 일부 코드 및 주석 수정(네 -> yes로 변경)

--- src/com/example/calculator/step2/App.java
+++ src/com/example/calculator/step2/App.java
@@ -2,6 +2,7 @@ package com.example.calculator.step2;
 
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -11,6 +12,8 @@ import java.util.Scanner;
  * Time: 오후 3:53
  **/
 // ?: 궁금했던 내용
+// !: 공부하면서 알게 된 내용
+// Step 2
 public class App {
     public static void main(String[] args) {
         // Calculator 인스턴스 생성
@@ -20,10 +23,30 @@ public class App {
 
         // 반복문 시작
         while (true) {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int num1 = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int num2 = sc.nextInt();
+            int num1, num2;   // !: 선언 필수 // ?: Step 1과 달리 Step 2에서는 왜 초기화 안해도 될까요?(아마 switch 문과 관련 있다고 생각중입니다...)
+
+            // 첫 번째 입력에서 숫자가 아니면 재입력 하는 기능 구현
+            while(true) {
+                try {
+                    System.out.print("첫 번째 숫자를 입력하세요: ");
+                    num1 = sc.nextInt();
+                    break;
+                } catch (InputMismatchException e) {
+                    System.out.println("숫자만 입력하세요!");
+                    sc.nextLine();  // !: 버퍼 제거 - 제거하지 않으면 무한루프 발생
+                }
+            }
+            // 두 번째 입력에서 숫자가 아니면 재입력 하는 기능 구현
+            while(true) {
+                try {
+                    System.out.print("두 번째 숫자를 입력하세요: ");
+                    num2 = sc.nextInt();
+                    break;
+                } catch (InputMismatchException e) {
+                    System.out.println("숫자만 입력하세요!");
+                    sc.nextLine();  // !: 버퍼 제거 - 제거하지 않으면 무한루프 발생
+                }
+            }
 
             System.out.print("사칙연산 기호를 입력하세요: ");
             char in = sc.next().charAt(0);
@@ -46,9 +69,9 @@ public class App {
             // 저장된 값 중 처음값 삭제 기능 구현
             while (true) {
                 if (!(newSave.isEmpty())) {
-                    System.out.println("저장된 값 중 가장 처음 값 삭제하시겠습니까? (네 입력시 삭제)");
+                    System.out.println("저장된 값 중 가장 처음 값 삭제하시겠습니까? (yes 입력시 삭제)");
                     String del = sc.next();
-                    if (del.equals("네")) {
+                    if (del.equals("yes")) {
                         cal.setDelete(0);
                         System.out.println("저장된 계산값: " + newSave);
                     } else {
