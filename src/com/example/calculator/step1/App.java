32b855b342cbc4038e3abbc9dca9412bdd63777b
jhsky3118 <doksakim7@naver.com>
2026-01-14 14:07:21 +0900

    refactor: Step 1 주석 내용 추가/삭제 및 변경

--- src/com/example/calculator/step1/App.java
+++ src/com/example/calculator/step1/App.java
@@ -12,9 +12,12 @@ import java.util.Scanner;
  **/
 // ?: 궁금했던 내용
 // !: 공부하면서 알게 된 내용
+// Step 1 하면서 중간중간에 궁금한 점들이 생겼는데 스스로 해결하면서 완성했습니다.
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 반복문 시작
         while (true) {
             int num1, num2 = 0;   // !: 선언 및 초기화 필수
 
@@ -42,15 +45,16 @@ public class App {
             }
 
             System.out.print("사칙연산 기호를 입력하세요: ");
-            String input = sc.next();   // ?: sc.nextLine() 을 쓰면 자동으로 엔터 입력되어 다음으로 넘어감
-            char in = input.charAt(0);  // ?: (char) 를 안써도 됨
+            String input = sc.next();   // !: sc.nextLine() 을 쓰면 자동으로 버퍼 입력되어 다음으로 넘어감
+            char in = input.charAt(0);  // !: .charAt 을 쓰면 String -> char 로 반환함
 
+            // 사칙연산 케이스에 따른 계산 기능 구현
             switch (in) {
                 case '+':
                     System.out.println("답: " + (num1 + num2));
                     break;
                 case '-':
-                    System.out.println("답: " + (num1 - num2)); // ?: 다른 사칙연산은 오류가 없는데 '-'만 ()를 안하면 오류가 나옴
+                    System.out.println("답: " + (num1 - num2)); // !: + 는 문자열 연결 연산자로도 사용되기 때문에 괄호()를 써줘야 함!
                     break;
                 case '*':
                     System.out.println("답: " + (num1 * num2));
@@ -62,15 +66,17 @@ public class App {
                     } catch (ArithmeticException e) {
                         System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
                         break;
-                    }   // 0으로 나눌 때 발생하는 오류의 예외 처리 기능 추가
+                    }
                 default:
                     System.out.println("올바른 사칙연산 기호중에 하나를 입력하시오.(+, -, *, /)");
             }
+
+            // 종료 확인 기능
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             String ex = sc.next();
             if (ex.equals("exit")) {
-                break;  // 반복문 종료(exit) 기능 추가
-            } // 반복문 완성
+                break; // 반복문 종료
+            }
         }
     }
 }
